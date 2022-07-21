11ea7fdf76fab32cc02f1aeec1e9f809935422e2
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-21 19:03:44 +0100

    Create, style and attach paragraph with DOM

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content); */
 
+const container = document.querySelector('#container');
+
+const paragraph = document.createElement('p');
+paragraph.style.color = 'red';
+paragraph.textContent = 'Hey I`m red!';
+
+
+container.appendChild(paragraph);
