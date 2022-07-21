9df21f1cb7408bffb389f4d363507e6744cff927
Lukasz Patalas <lukasz.patalas86@gmail.com>
2022-07-21 18:58:21 +0100

    Move js scripts into head

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const container = document.querySelector('#container');
+/* const container = document.querySelector('#container');
 
 const content = document.createElement('div');
 content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
-container.appendChild(content);
+container.appendChild(content); */
+
