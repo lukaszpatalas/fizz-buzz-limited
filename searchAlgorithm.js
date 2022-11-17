8f459cc7982266ab4869bc50a89bb915fa84fc3b
lukaszpatalas <lukasz.patalas86@gmail.com>
2022-11-17 15:47:51 +0100

    Add initial code to be conversed into class

--- searchAlgorithm.js
+++ searchAlgorithm.js
@@ -1,19 +1,22 @@
-let head = {
-    glasses: 1
-  };
-  
-  let table = {
-    pen: 3,
-    __proto__: head
-  };
-  
-  let bed = {
-    sheet: 1,
-    pillow: 2,
-    __proto__: table
-  };
-  
-  let pockets = {
-    money: 2000,
-    __proto__: bed
-  };
+let hamster = {
+    eat(food) {
+        this.stomach.push(food);
+    }
+};
+
+let speedy = {
+    stomach: [],
+    __proto__: hamster
+};
+
+let lazy = {
+    stomach: [],
+    __proto__: hamster
+};
+
+// This one found the food
+speedy.eat("apple");
+alert(speedy.stomach); // apple
+
+// This one also has it, why? fix please.
+alert(lazy.stomach); // apple
